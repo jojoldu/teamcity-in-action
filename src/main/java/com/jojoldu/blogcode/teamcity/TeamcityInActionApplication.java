950ddc7d91f77972daeba8277b92fd0608605040
이동욱 <jojoldu@gmail.com>
2019-09-07 23:59:39 +0900

    생성

---
+++ src/main/java/com/jojoldu/blogcode/teamcity/TeamcityInActionApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcityInActionApplication.class, args);
+    }
+
+}
